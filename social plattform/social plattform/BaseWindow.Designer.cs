5547fbdb7d7806a0cb53a5a030239a19c2adcf84
Eric Ekstrm <ericekstrm@gmail.com>
2015-08-17 19:15:19 +0200

    First GUI Draft

---
+++ social plattform/social plattform/BaseWindow.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace social_plattform
+{
+    partial class BaseWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BaseWindow));
+            this.MainMenuPanel = new System.Windows.Forms.Panel();
+            this.Logo = new System.Windows.Forms.PictureBox();
+            this.MyWallLabel = new System.Windows.Forms.Label();
+            this.ProfileLabel = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.MainMenuPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainMenuPanel
+            // 
+            this.MainMenuPanel.Controls.Add(this.ProfileLabel);
+            this.MainMenuPanel.Controls.Add(this.MyWallLabel);
+            this.MainMenuPanel.Controls.Add(this.Logo);
+            this.MainMenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MainMenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainMenuPanel.Name = "MainMenuPanel";
+            this.MainMenuPanel.Size = new System.Drawing.Size(150, 612);
+            this.MainMenuPanel.TabIndex = 0;
+            // 
+            // Logo
+            // 
+            this.Logo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Logo.BackgroundImage")));
+            this.Logo.InitialImage = null;
+            this.Logo.Location = new System.Drawing.Point(15, 15);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(120, 120);
+            this.Logo.TabIndex = 0;
+            this.Logo.TabStop = false;
+            this.Logo.Click += new System.EventHandler(this.Logo_Click);
+            // 
+            // MyWallLabel
+            // 
+            this.MyWallLabel.AutoSize = true;
+            this.MyWallLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MyWallLabel.Location = new System.Drawing.Point(15, 150);
+            this.MyWallLabel.Name = "MyWallLabel";
+            this.MyWallLabel.Size = new System.Drawing.Size(109, 31);
+            this.MyWallLabel.TabIndex = 1;
+            this.MyWallLabel.Text = "My Wall";
+            this.MyWallLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ProfileLabel
+            // 
+            this.ProfileLabel.AutoSize = true;
+            this.ProfileLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProfileLabel.Location = new System.Drawing.Point(15, 196);
+            this.ProfileLabel.Name = "ProfileLabel";
+            this.ProfileLabel.Size = new System.Drawing.Size(91, 31);
+            this.ProfileLabel.TabIndex = 2;
+            this.ProfileLabel.Text = "Profile";
+            this.ProfileLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // BaseWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 612);
+            this.Controls.Add(this.MainMenuPanel);
+            this.Name = "BaseWindow";
+            this.Text = "Super Awesome Stoff";
+            this.Load += new System.EventHandler(this.BaseWindow_Load);
+            this.MainMenuPanel.ResumeLayout(false);
+            this.MainMenuPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainMenuPanel;
+        private System.Windows.Forms.PictureBox Logo;
+        private System.Windows.Forms.Label MyWallLabel;
+        private System.Windows.Forms.Label ProfileLabel;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
